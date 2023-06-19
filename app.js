aa7fbd9957806a0c53358ad99c02256814892206
KushalRaut <kushal.raut@kotuko.it>
2023-06-19 17:22:20 +0545

    feat: Setting to create complete CLI application

--- app.js
+++ app.js
@@ -1,71 +1,143 @@
+const chalk = require("chalk");
+const yargs = require("yargs");
+const notes = require("./notes.js");
+const inquirer = require("inquirer");
 
-const chalk = require('chalk')
-const yargs = require('yargs')
-const notes = require('./notes.js')
+yargs.version("1.1.0");
 
-yargs.version('1.1.0')
-
-yargs.command({
-    command:'add',
-    describe:'add a new note',
-    builder:{
-        title:{
-            describe:'Note title',
-            demandOption:true,
-            type:'string'
-        },
-        body:{
-            describe:'The actual note',
-            demandOption:true,
-            type:'string'
-        }
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "operation",
+      message: "What do you want to do?",
+      choices: [
+        "1. Create a note",
+        "2. Read all notes",
+        "3. Update a note",
+        "4. Delete a note",
+      ],
     },
-    handler(argv){
-        notes.addNotes(argv.title,argv.body)
-    }
-})
-yargs.command({
-    command:'remove',
-    describe:'delete a note',
-    builder:{
-        title:{
-            describe:'notes title',
-            demandOption:true,
-            type:'string'
-        }
-    },
-    handler(argv){
-        notes.removeNotes(argv.title);
-    }
-
-})
-yargs.command({
-
-    command:'list',
-    describe:'List all of your notes',
-    handler(){
+  ])
+  .then((answers) => {
+    const choice = answers.operation.substring(0, 1);
+    switch (choice) {
+      case "1":
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "title",
+              message: "Enter the title of note",
+            },
+            {
+              type: "input",
+              name: "body",
+              message: "Enter the body of note",
+            },
+          ])
+          .then((answers) => {
+            notes.addNotes(answers.title, answers.body);
+          });
+        break;
+      case "2":
         notes.listNotes();
+        break;
+      case "3":
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "title",
+              message: "Enter the title of note",
+            },
+            {
+              type: "input",
+              name: "body",
+              message: "Enter the body of note",
+            },
+          ])
+          .then((answers) => {
+            notes.updateNotes(answers.title, answers.body);
+          });
+        break;
+      case "4":
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "title",
+              message: "Enter the title of note",
+            },
+          ])
+          .then((answers) => {
+            notes.removeNotes(answers.title);
+          });
+        break;
+      default:
+        console.log("Invalid choice");
     }
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
 
-})
-    
 yargs.command({
-    command:'read',
-    describe:'this is going to read your notes',
-    builder:{
-        title:
-        {   
-            describe:'Give the title of note you want to read',
-            demandOption: true,
-            type:'string'
-
-        }
+  command: "add",
+  describe: "add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
     },
-    handler(argv){
-        notes.readNotes(argv.title);
-    }
-    
-})
+    body: {
+      describe: "The actual note",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.addNotes(argv.title, argv.body);
+  },
+});
+yargs.command({
+  command: "remove",
+  describe: "delete a note",
+  builder: {
+    title: {
+      describe: "notes title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.removeNotes(argv.title);
+  },
+});
+yargs.command({
+  command: "list",
+  describe: "List all of your notes",
+  handler() {
+    notes.listNotes();
+  },
+});
 
-console.log(yargs.argv)
-
+yargs.command({
+  command: "read",
+  describe: "this is going to read your notes",
+  builder: {
+    title: {
+      describe: "Give the title of note you want to read",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.readNotes(argv.title);
+  },
+});
--- notes.js
+++ notes.js
@@ -1,82 +1,84 @@
-const { default: chalk } = require('chalk');
-const fs = require('fs')
+const { default: chalk } = require("chalk");
+const fs = require("fs");
+const { uid } = require("uid");
 
-const getNotes =  () => {
-    return ('your notes...')
-}
+const getNotes = () => {
+  return "your notes...";
+};
 
-const addNotes =  (title,body) => {
-    const notes = loadNotes();
-    const duplicateNotes = notes.find((note) => { note.title===title})
+const addNotes = (title, body) => {
+  const notes = loadNotes();
+  const duplicateNotes = notes.find((note) => {
+    note.title === title;
+  });
 
-    if (!duplicateNotes){
-        notes.push({
-            title:title,
-            body:body
-        })
-    
-        saveNotes(notes)
-        console.log(chalk.green.inverse('Notes added'))
-    }else{
-        console.log('Title is already taken')
-    }
-    
-}
+  if (!duplicateNotes) {
+    notes.push({
+      uid: uid(16),
+      title: title,
+      body: body,
+    });
+
+    saveNotes(notes);
+    console.log(
+      chalk.green.bold("Note added with ", "Title: ", title, "Body: ", body)
+    );
+  } else {
+    console.log("Title is already taken");
+  }
+};
 
 const removeNotes = (title) => {
-    const notes = loadNotes();
+  const notes = loadNotes();
 
-    const updatedNotes = notes.filter((note) => {
-        return (note.title!=title)
-    })
-    if(updatedNotes.length != notes.length ){
-        saveNotes(updatedNotes);
-        console.log('Note deleted successfully')
-    }
-    else{
-        console.log('There is no such title in Notes file')
-    }
+  const updatedNotes = notes.filter((note) => {
+    return note.title != title;
+  });
+  if (updatedNotes.length != notes.length) {
+    saveNotes(updatedNotes);
+    console.log("Note deleted successfully");
+  } else {
+    console.log("There is no such title in Notes file");
+  }
+};
 
-}
-    
 const saveNotes = (notes) => {
-    const dataJSON =JSON.stringify(notes)
-    fs.writeFileSync('notes.json',dataJSON)
-}
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
 
-const loadNotes = () =>  {
-    try{
-        const dataBuffer = fs.readFileSync('notes.json')
-        const dataJson = dataBuffer.toString();
-        return JSON.parse(dataJson);
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJson = dataBuffer.toString();
+    return JSON.parse(dataJson);
+  } catch (e) {
+    return [];
+  }
+};
 
-    }catch(e){
-        return []
-    }    
-}
-
-const listNotes = ()=>{
-    const notes = loadNotes();
-    console.log("Title","Body")
-    notes.forEach(note => {
-        console.log(note.title,note.body)
-    });
-}
+const listNotes = () => {
+  const notes = loadNotes();
+  console.log(chalk.bgCyan.bold("Title"), chalk.bgGreen("Body"));
+  notes.forEach((note) => {
+    console.log(chalk.cyan.bold(note.title), chalk.green(note.body));
+  });
+};
 
 const readNotes = (title) => {
-    const notes = loadNotes();
-    const readNote = notes.find((note)=> note.title == title)
-    if(!readNote){
-        console.log(chalk.red.inverse('No note found'))
-    }else{
-        console.log(title,readNote.body)
-    }
-}
+  const notes = loadNotes();
+  const readNote = notes.find((note) => note.title == title);
+  if (!readNote) {
+    console.log(chalk.red.inverse("No note found"));
+  } else {
+    console.log(title, readNote.body);
+  }
+};
 
 module.exports = {
-    getNotes: getNotes,
-    addNotes: addNotes,
-    removeNotes: removeNotes,
-    listNotes: listNotes,
-    readNotes:readNotes
-}
+  getNotes: getNotes,
+  addNotes: addNotes,
+  removeNotes: removeNotes,
+  listNotes: listNotes,
+  readNotes: readNotes,
+};
